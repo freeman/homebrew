936512a6cada61cb99f27746c1fca55af95e6de8
Michel Rasschaert <michel.rasschaert@gmail.com>
2010-04-30 23:15:57 +0200

    added iconv support

--- Library/Formula/rsync.rb
+++ Library/Formula/rsync.rb
@@ -1,20 +1,20 @@
 require 'formula'
 
 class Rsync <Formula
+  head 'git://git.samba.org/rsync.git'
   url 'http://rsync.samba.org/ftp/rsync/rsync-3.0.7.tar.gz'
   homepage 'http://rsync.samba.org/'
   md5 'b53525900817cf1ba7ad3a516ab5bfe9'
 
-  def patches
-    base = "http://trac.macports.org/export/65950/trunk/dports/net/rsync/files"
-    {
-      :p1 => ["#{base}/patch-fileflags.diff", "#{base}/patch-crtimes.diff"]
-    }
-  end
+  depends_on 'libiconv'
 
   def install
-    system "./configure", "--disable-debug",
-                          "--prefix=#{prefix}",
+  iconv = Formula.factory 'libiconv'
+  ENV.append "CFLAGS",  "-I#{iconv.prefix}/include"
+  ENV.append "LDFLAGS", "-L#{iconv.prefix}/lib"
+
+    system "./configure", "--prefix=#{prefix}",
+                          "--disable-debug",
                           "--with-rsyncd-conf=#{prefix}/etc/rsyncd.conf",
                           "--enable-ipv6"
     system "make install"
